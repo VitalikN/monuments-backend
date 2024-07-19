19706fc3036878743811b6ee9e016a0db8d5125e
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2024-07-19 20:28:05 +0300

    fix dbConnection

--- index.js
+++ index.js
@@ -3,13 +3,43 @@ const mongoose = require('mongoose');
 
 const { DB_HOST, PORT } = process.env;
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(PORT);
-    console.log('Database connection successful');
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
+if (!DB_HOST) {
+  throw new Error('DB_HOST is not defined');
+}
+
+console.log('Attempting to connect to MongoDB...');
+
+const dbConnection = async () =>
+  mongoose
+    .connect(DB_HOST, {})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => {
+      console.log('MongoDB connection error:', error);
+      process.exit(1);
+    });
+
+const startServer = () => {
+  app.listen(PORT || 3000, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
+};
+
+const init = async () => {
+  await dbConnection();
+  startServer();
+};
+
+init();
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     app.listen(PORT);
+//     console.log('Database connection successful');
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
+//
+//
